867c243eac0116d51d6e933112b4605647e4cf52
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2017-11-18 17:39:22 -0800

    Update main.py

--- pybank/main.py
+++ pybank/main.py
@@ -1,30 +1,34 @@
 #PyBank
-#Part 1: Sum all revenue values and return a single total.
-
 import os, csv
 csvpath = os.path.join('budget_data_1.csv')
 
 with open(csvpath) as f:
     readcsv = csv.reader(f, delimiter=',')
 
-    #Part 1: Sum all revenue values and return a single total.
+    next(readcsv)
+
+    revenue_list = []
+    transaction_list = []
+
     revtotal = 0
+
     for row in readcsv:
         revtotal = revtotal + int(row[1])
-    print(total)  #Need to figure out how to exclude the header from the total
+        revenue_list.append(row[1])
+        transaction_list.append(row[0])
 
-    #Part 2: Determine the total number of transactions in the data set.
-    monthtotal = 0
-    for row in readcsv:
-        monthtotal += 1
-    print(monthtotal - 1)
+    print(revenue_list)
+    print(transaction_list)
 
+    print("Financial Summary")
+    print("----------------------------------------------------------")
+    print("Total Revenue: ", revtotal)
+    print("Minimin Revenue: ", min(revenue_list))
+    print("Maximum Revneue: ", max(revenue_list))
+    print("Total Number of Transactions: ", len(transaction_list))
+    print("Average: ", float(int(revtotal)/int(len(transaction_list))))
 
-    #Part X: Print the summary table.
-    print("Financial Analysis")
-    print("----------------------------")
-    print("Total Months: ")
-    print("Total Revenue: ")
-    print("Average Revenue Change: ")
-    print("Greatest Increse in Revenue: ")
-    print("Greatest Decrese in Revenue: ")
+    #Need to find maximum value from revenue list and return the index.
+    #Then I need to pull out the corresponding month in the transaction list at the same index.
+
+    print(transaction_list[revenue_list.index(max(revenue_list))])
