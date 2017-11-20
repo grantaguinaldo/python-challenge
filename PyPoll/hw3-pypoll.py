427c3728b8bfb0569f95d1c5aaa5211e68bdec0a
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2017-11-19 23:10:06 -0800

    2017-11-19 commit.

--- PyPoll/hw3-pypoll.py
+++ PyPoll/hw3-pypoll.py
@@ -1,29 +1,39 @@
+#Standard Imports
 import os, csv
 
+#Directory of the CSV file.
 filepath = os.path.join("election_data_1.csv")
 
+#Open the CSV file and read the data while skipping the first row (that contains the headers)
 with open(filepath) as f:
     readcsv = csv.reader(f,delimiter=",")
+
     next(readcsv)
-
     #Count the number of voters that cast votes.
-    num_votes_cast = sum([1 for row in readcsv])
-    print(num_votes_cast)
+    num_votes_cast = []
+    for row in readcsv:
+        num_votes_cast.append(1)
+    total_votes_cast = sum(num_votes_cast)
+    print("Number of votes casted: ", total_votes_cast)
 
+    f.seek(0)
+    next(readcsv)
     #Return a list of unique canidates.
     canidate_list_all = [row[2] for row in readcsv]
     canidate_list_unique = []
     for i in canidate_list_all:
         if i not in canidate_list_unique:
             canidate_list_unique.append(i)
-    #Print(canidate_list_unique) #['Vestal', 'Torres', 'Seth', 'Cordin']
+    print(canidate_list_unique) #['Vestal', 'Torres', 'Seth', 'Cordin']
 
-    #Determine the number of votes for each canidate.
+    f.seek(0)
+    next(readcsv)
+    #Determine the number of votes for each canidate by summing.
     votes_vestal = 0
     votes_torres = 0
     votes_seth = 0
     votes_cordin = 0
-    votes_total = 0
+    votes_total = sum(num_votes_cast)
 
     for row in readcsv:
         if row[2] == "Vestal":
@@ -37,8 +47,25 @@ with open(filepath) as f:
         else:
             votes_cordin += 1
 
-    print(votes_vestal)
-    print(votes_torres)
+    #Determine the percentages for each canidate.
+    pct_votes_vestal = (float(votes_vestal/votes_total)) * 100
+    pct_votes_torres = (float(votes_torres/votes_total)) * 100
+    pct_votes_seth = (float(votes_seth/votes_total)) * 100
+    pct_votes_cordin = (float(votes_cordin/votes_total)) * 100
+
+    print("Seth Received " , pct_votes_seth, "%")
+    print(pct_votes_cordin , "%")
+    print(pct_votes_torres, "%")
+    print(pct_votes_vestal, "%")
+
+    print("---")
     print(votes_seth)
     print(votes_cordin)
-    print(votes_seth + votes_torres + votes_cordin + votes_vestal)
+    print(votes_torres)
+    print(votes_vestal)
+
+    #Notes.
+    #Create dictionary with results.
+    #Send individual results to a list or dictionary and pair with the canidates.
+    #Figure out how to format the data so that it shows up like the example.
+    #Figure out how to get all lists in a the for loops to print.
